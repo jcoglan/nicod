6d204023330531ba8be4154543aea6bd74c9c998
James Coglan <jcoglan@gmail.com>
2019-11-08 17:21:20 +0100

    Benchmarks should not measure the time to build expressions
    By using the `unify!` macro, we're making the code inside `bench.iter(||
    ...)` build new instances of the unified expressions on every iteration.
    We actually only want to measure the performance of `State::unify`,
    which will typically operate on references to existing expressions, so
    we extract the building of expressions from the benchmark loop.
    
    New representative numbers for the benchmarks:
    
        test nested_seqs                    ... bench:       7,690 ns/iter (+/- 1,534)
        test nested_seqs_with_indirect_vars ... bench:      60,509 ns/iter (+/- 3,008)
        test seqs_with_vars                 ... bench:      10,575 ns/iter (+/- 3,467)
        test var                            ... bench:       1,935 ns/iter (+/- 122)
        test words                          ... bench:         830 ns/iter (+/- 52)

--- benches/unify.rs
+++ benches/unify.rs
@@ -8,84 +8,84 @@ use nicod::*;
 
 #[bench]
 fn words(bench: &mut Bencher) {
-    bench.iter(|| unify!(wrd(a), wrd(a)));
+    let expr_a = expr!(wrd(a));
+    let expr_b = expr!(wrd(a));
+
+    bench.iter(|| State::new().unify(&expr_a, &expr_b));
 }
 
 #[bench]
 fn var(bench: &mut Bencher) {
-    bench.iter(|| unify!(wrd(a), var(x)));
+    let expr_a = expr!(wrd(a));
+    let expr_b = expr!(var(x));
+
+    bench.iter(|| State::new().unify(&expr_a, &expr_b));
 }
 
 #[bench]
 fn seqs_with_vars(bench: &mut Bencher) {
-    bench.iter(|| {
-        unify!(
-            seq(wrd(a), var(w), wrd(b), var(x)),
-            seq(var(y), wrd(c), var(z), wrd(d))
-        )
-    });
+    let expr_a = expr!(seq(wrd(a), var(w), wrd(b), var(x)));
+    let expr_b = expr!(seq(var(y), wrd(c), var(z), wrd(d)));
+
+    bench.iter(|| State::new().unify(&expr_a, &expr_b));
 }
 
 #[bench]
 fn nested_seqs(bench: &mut Bencher) {
-    bench.iter(|| {
-        unify!(
+    let expr_a = expr!(seq(
+        seq(wrd(a), wrd(b)),
+        wrd(c),
+        seq(
+            wrd(d),
             seq(
-                seq(wrd(a), wrd(b)),
-                wrd(c),
-                seq(
-                    wrd(d),
-                    seq(
-                        seq(wrd(e)),
-                        seq(wrd(f), wrd(g)),
-                        seq(seq(seq(wrd(h)), wrd(i)), wrd(j))
-                    )
-                )
-            ),
-            seq(
-                seq(wrd(a), wrd(b)),
-                wrd(c),
-                seq(
-                    wrd(d),
-                    seq(
-                        seq(wrd(e)),
-                        seq(wrd(f), wrd(g)),
-                        seq(seq(seq(wrd(h)), wrd(i)), wrd(j))
-                    )
-                )
+                seq(wrd(e)),
+                seq(wrd(f), wrd(g)),
+                seq(seq(seq(wrd(h)), wrd(i)), wrd(j))
             )
         )
-    });
+    ));
+    let expr_b = expr!(seq(
+        seq(wrd(a), wrd(b)),
+        wrd(c),
+        seq(
+            wrd(d),
+            seq(
+                seq(wrd(e)),
+                seq(wrd(f), wrd(g)),
+                seq(seq(seq(wrd(h)), wrd(i)), wrd(j))
+            )
+        )
+    ));
+
+    bench.iter(|| State::new().unify(&expr_a, &expr_b));
 }
 
 #[bench]
 fn nested_seqs_with_indirect_vars(bench: &mut Bencher) {
-    bench.iter(|| {
-        unify!(
+    let expr_a = expr!(seq(
+        seq(var(z), var(w)),
+        var(r),
+        seq(
+            var(u),
             seq(
-                seq(var(z), var(w)),
-                var(r),
-                seq(
-                    var(u),
-                    seq(
-                        seq(var(r)),
-                        seq(wrd(f), var(w)),
-                        seq(seq(seq(var(u)), var(t)), var(x))
-                    )
-                )
-            ),
-            seq(
-                seq(var(y), var(x)),
-                var(s),
-                seq(
-                    var(v),
-                    seq(
-                        seq(var(q)),
-                        seq(var(q), var(v)),
-                        seq(seq(seq(var(t)), var(s)), var(y))
-                    )
-                )
+                seq(var(r)),
+                seq(wrd(f), var(w)),
+                seq(seq(seq(var(u)), var(t)), var(x))
             )
         )
-    });
+    ));
+    let expr_b = expr!(seq(
+        seq(var(y), var(x)),
+        var(s),
+        seq(
+            var(v),
+            seq(
+                seq(var(q)),
+                seq(var(q), var(v)),
+                seq(seq(seq(var(t)), var(s)), var(y))
+            )
+        )
+    ));
+
+    bench.iter(|| State::new().unify(&expr_a, &expr_b));
 }
