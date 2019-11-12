c15f929870d75744c724e498157e3af41cdf6c86
James Coglan <jcoglan@gmail.com>
2019-11-12 09:29:06 +0000

    Implement `Debug` for `Expr`
    This gives us a `Debug` representation of expressions that's closer to
    what we'll end up with as user-facing syntax. Words are displayed as
    just their names, variables as their names with a leading `$`, and
    sequences as their elements separated by spaces, surround in parens for
    nested sequences.

--- src/expr.rs
+++ src/expr.rs
@@ -1,17 +1,53 @@
+use std::fmt;
 use std::rc::Rc;
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub enum Expr {
     Var(Variable),
     Wrd(Word),
     Seq(Sequence),
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+impl fmt::Debug for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Expr::Var(var) => var.fmt(f),
+            Expr::Wrd(wrd) => wrd.fmt(f),
+            Expr::Seq(seq) => seq.fmt(f),
+        }
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Hash)]
 pub struct Variable(pub String);
 
-#[derive(Debug, Clone, PartialEq)]
+impl fmt::Debug for Variable {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "${}", self.0)
+    }
+}
+
+#[derive(Clone, PartialEq)]
 pub struct Word(pub String);
 
-#[derive(Debug, Clone, PartialEq)]
+impl fmt::Debug for Word {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+#[derive(Clone, PartialEq)]
 pub struct Sequence(pub Vec<Rc<Expr>>);
+
+impl fmt::Debug for Sequence {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "(")?;
+        for (i, expr) in self.0.iter().enumerate() {
+            expr.fmt(f)?;
+            if i < self.0.len() - 1 {
+                write!(f, " ")?;
+            }
+        }
+        write!(f, ")")
+    }
+}
