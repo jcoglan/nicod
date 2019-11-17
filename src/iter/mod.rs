f6a1448b0eb87ec5703646e01d17ba127e883ff4
James Coglan <jcoglan@gmail.com>
2019-11-17 11:55:52 +0000

    Flatten infinite streams of infinite streams
    A few commits ago we introduced the `Interleave` type, which could
    interleave a finite set of infinite streams. However in general we
    actually need to be able to flatten *infinite sets of infinite streams*.
    
    If a certain rule generates an infinite set of states in which it
    matches the target expression, *each* of those states will in turn be
    used as the basis to try to derive further expressions, and each of
    those derivations may produce an infinite stream.
    
    For example, imagine a rule has premises A and B and we try to derive A
    in some state S, and this produces an infinite stream of states S'. We
    will then try to derive B in *each state* in S', and each of *those*
    derivations of B may themselves return an infinite stream. In order for
    the rule as a whole to return `Iterator<Item=State>`, we need to flatten
    these streams.
    
    This is effectively Hilbert's Hotel [1]. We can solve it by naively
    interleaving the first stream with the result of flattening all the
    following streams, i.e.:
    
        flatten []     = []
        flatten (x:xs) = interleave x (flatten xs)
    
    Or to put it another way:
    
        streams.fold([], |a, b| interleave(a, b))
    
    The problem is that in Rust, `fold()` is not lazy, and that recursive
    call to `interleave()` needs to be deferred otherwise it will cause a
    stack overflow. `iter::Flatten` takes care of this by using the `Inner`
    struct to defer the execution of interleaving two streams.
    
    [1]: https://en.wikipedia.org/wiki/Hilbert%27s_paradox_of_the_Grand_Hotel

--- src/iter/mod.rs
+++ src/iter/mod.rs
@@ -1,6 +1,71 @@
 mod tests;
 
-type BoxIter<'a, T> = Box<dyn Iterator<Item = T> + 'a>;
+use std::mem;
+
+pub type BoxIter<'a, T> = Box<dyn Iterator<Item = T> + 'a>;
+
+pub struct Flatten<'a, T> {
+    inner: Inner<'a, T>,
+}
+
+impl<'a, T> Flatten<'a, T> {
+    pub fn new<I, J>(streams: I) -> Flatten<'a, T>
+    where
+        I: IntoIterator<Item = J> + 'a,
+        J: IntoIterator<Item = T> + 'a,
+    {
+        let iters = streams
+            .into_iter()
+            .map(|s| Box::new(s.into_iter()) as BoxIter<'a, T>);
+
+        let inner = Inner::Pending(Some(Box::new(iters)));
+        Flatten { inner }
+    }
+}
+
+impl<'a, T: 'a> Iterator for Flatten<'a, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next()
+    }
+}
+
+enum Inner<'a, T> {
+    Pending(Option<BoxIter<'a, BoxIter<'a, T>>>),
+    Forced(Interleave<'a, T>),
+}
+
+impl<'a, T: 'a> Inner<'a, T> {
+    fn force(&mut self) {
+        if let Inner::Pending(streams) = self {
+            if let Some(streams) = streams.take() {
+                self.expand_streams(streams);
+            }
+        }
+    }
+
+    fn expand_streams(&mut self, mut streams: BoxIter<'a, BoxIter<'a, T>>) {
+        if let Some(head) = streams.next() {
+            let tail = Box::new(Inner::Pending(Some(streams)));
+            let merged = Interleave::new(vec![head, tail]);
+            mem::replace(self, Inner::Forced(merged));
+        }
+    }
+}
+
+impl<'a, T: 'a> Iterator for Inner<'a, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.force();
+
+        match self {
+            Inner::Forced(stream) => stream.next(),
+            _ => None,
+        }
+    }
+}
 
 pub struct Interleave<'a, T> {
     cursor: usize,
--- src/iter/tests.rs
+++ src/iter/tests.rs
@@ -3,6 +3,49 @@
 use super::*;
 
 #[test]
+fn flatten_single_iterator() {
+    let streams = vec![vec![1, 2, 3, 4]];
+
+    let flattened: Vec<_> = Flatten::new(streams).collect();
+    assert_eq!(flattened, vec![1, 2, 3, 4]);
+}
+
+#[test]
+fn flatten_two_iterators() {
+    let streams = vec![vec![1, 2, 3, 4], vec![5, 6, 7]];
+
+    let flattened: Vec<_> = Flatten::new(streams).collect();
+    assert_eq!(flattened, vec![1, 5, 2, 6, 3, 7, 4]);
+}
+
+#[test]
+fn flatten_many_iterators() {
+    let streams = vec![vec![1, 2], vec![3], vec![4, 5, 6, 7], vec![8, 9, 10]];
+
+    let flattened: Vec<_> = Flatten::new(streams).collect();
+    assert_eq!(flattened, vec![1, 3, 2, 4, 8, 5, 9, 6, 10, 7]);
+}
+
+#[test]
+fn flatten_infinite_ranges() {
+    let streams = vec![0.., 10.., 20.., 30..];
+
+    let flattened: Vec<_> = Flatten::new(streams).take(10).collect();
+    assert_eq!(flattened, vec![0, 10, 1, 20, 2, 11, 3, 30, 4, 12]);
+}
+
+#[test]
+fn flatten_infinite_stream_of_ranges() {
+    let streams = (0..).map(|n| (100 * n)..);
+
+    let flattened: Vec<_> = Flatten::new(streams).take(20).collect();
+    assert_eq!(
+        flattened,
+        vec![0, 100, 1, 200, 2, 101, 3, 300, 4, 102, 5, 201, 6, 103, 7, 400, 8, 104, 9, 202]
+    );
+}
+
+#[test]
 fn interleave_single_iterator() {
     let streams = vec![vec![1, 2, 3, 4]];
 
